81045797c854490888fe0cef47e8a13f8b608f09
norasalgo <nora.salgo@uzh.ch>
2017-06-16 14:44:38 +0200

    Uth

--- script.py
+++ script.py
@@ -16,15 +16,20 @@ k_B = 1.38064852*10**(-23)
 
 #print(np.exp(1.107*eV/(2*k_B*300))/10000)
 
-V = data[:,0]
-R = data[:,1]
-T = data[:,2]
-t = data[:,3]
+V = data[:,0] #mV
+R = data[:,1] #Ohm
+U1 = data[:,2] #V
+t = data[:,3] #s
 
 A_B_guess = [1/10000,1.107*eV/(2*k_B)]
 
 def func(T,A,B):
     return A*np.exp(B/T)
+
+def Uth (u1, t0, t1):
+    uv = u1*t0/t1
+    return u1 - uv
+
 
 param = scipy.optimize.curve_fit(func,T,R,A_B_guess)[0]
 
