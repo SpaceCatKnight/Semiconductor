e11f835988e6ee95cd0e2bd34b07efa9b9f37b27
SpaceCatKnight <m.r.sommerhalder@hotmail.com>
2017-06-16 17:10:36 +0200

    Pushpull

--- script.py
+++ script.py
@@ -5,6 +5,12 @@ Created on Fri Jun 16 12:29:36 2017
 @author: mrsom
 """
 
+'''
+- in eigenen Worten zeigen, dass wir 4-Punktmessung und PID-Regler verstanden haben
+- erklären, wieso man Vakuum braucht
+- Fehler qualitativ nennen (nicht ganz intrinsisch, Raumtemperatur geraten, übliches)
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize
@@ -33,11 +39,11 @@ T = 273.15+np.array(T_C) #K
 
 T_0 = 26
 
-A_B_guess = [10,1.107*eV/(2*k_B)]
+A_B_guess = [100,1.107*eV/(2*k_B)]
 
 
 def func(T,A,B):
-    return A*T**(-3/2)*np.exp(-B/T)
+    return A*T**(-3/2)*np.exp(B/T)
     
 T_1 = tabelle(V,a,b)
 
@@ -47,16 +53,33 @@ def Uth(V, T_0,a,b):
 
 T_korr = tabelle(Uth(V,T_0,a,b),a,b)
 
-param = scipy.optimize.curve_fit(func,T,R)[0]
+T_korr = 273.15+np.array(T_korr)
+
+param = scipy.optimize.curve_fit(func,T,R,A_B_guess)[0]
 
 A_new = float(param[0])
 B_new = float(param[1])
 print(A_new)
 print(B_new)
 
-
+'''
 plt.figure()
 plt.plot(T,R,'r')
-plt.plot(T,func(T,*A_B_guess),'g')
+#plt.plot(T,func(T,*A_B_guess),'g')
 plt.plot(T,func(T,A_new,B_new))
 plt.show()
+'''
+
+
+def linearfit(x,a,b):
+    return a*x + b
+
+guess = [1.107*eV/(2*k_B),1]
+Tfit= T_korr[400:1200]
+Rfit=R[400:1200]
+a,b = scipy.optimize.curve_fit(linearfit,1/Tfit,np.log(Rfit),guess)[0]
+
+plt.plot(1/T_korr,np.log(R))
+plt.plot(1/Tfit, linearfit(1/Tfit,a,b))
+
+print(a*2*k_B/eV)
