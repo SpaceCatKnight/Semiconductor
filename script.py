7224bcb9c6ba26f3e0dff9c26fca4eea403bc1a3
SpaceCatKnight <m.r.sommerhalder@hotmail.com>
2017-06-16 14:30:01 +0200

    blabla

--- script.py
+++ script.py
@@ -11,12 +11,17 @@ import scipy.optimize
 
 data = np.loadtxt('data_test.txt')
 
+eV = 1.6021766208*10**(-19)
+k_B = 1.38064852*10**(-23)
+
+#print(np.exp(1.107*eV/(2*k_B*300))/10000)
+
 V = data[:,0]
 R = data[:,1]
 T = data[:,2]
 t = data[:,3]
 
-A_B_guess = [1000000000,0.00000000001]
+A_B_guess = [1/10000,1.107*eV/(2*k_B)]
 
 def func(T,A,B):
     return A*np.exp(B/T)
