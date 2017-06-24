16280c7c691bf63844b0ca3c0b9bd44983af43aa
SpaceCatKnight <m.r.sommerhalder@hotmail.com>
2017-06-24 11:39:22 +0200

    update: script.py und neu: data visialization.py
    script.py lediglich aufgeräumt und mit Kommentaren übersichtlicher gestaltet.
    data visualization dient primär dazu, den vollständigen Datensatz auszulesen und in einen Heat- und einen Cool-Bereich aufzutrennen.

---
+++ data visualization.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 24 10:22:41 2017
+
+@author: mrsom
+"""
+
+'''Der Peak von T und der von V liegen ca. 5s auseinander.
+T hat gegenueber V den Vorteil, dass das Maximum nur einmal erreicht wird, statt 20mal
+'''
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize
+
+
+data1 = np.loadtxt(fname='Full R data.txt', skiprows=7)
+
+V = data1[:,0]
+R = data1[:,1]
+T = data1[:,2]
+t = data1[:,3]
+
+#data2 = np.loadtxt(fname='Full PID data.txt', skiprows=6)
+
+
+#Ausreisser ausglätten
+Vmin = np.min(V)
+Vmax = np.max(V)
+for i in range(len(V)):
+    if V[i] == Vmin:
+        V[i] = V[i-1]
+#    if V[i] == Vmax:
+#        print('Vmax at t = ', t[i])
+    
+#Cool- und Heat-Teil voneinander trennen
+Tmax = np.max(T)
+for i in range(len(T)):
+    if T[i] == Tmax:
+#        print('Tmax at t = ', t[i])
+        V_heat = V[:i]
+        V_cool = V[i:]
+        R_heat = R[:i]
+        R_cool = R[i:]       
+        T_heat = T[:i]
+        T_cool = T[i:]
+        t_heat = t[:i]
+        t_cool = t[i:]
+
+
+plt.subplot(221)
+plt.plot(T_heat,R_heat,'r')
+
+plt.subplot(222)
+plt.plot(V_heat,R_heat,'b')
+
+plt.subplot(223)
+plt.plot(T_cool,R_cool,'g')
+
+plt.subplot(224)
+plt.plot(V_cool,R_cool,'y')
+
--- script.py
+++ script.py
@@ -16,19 +16,19 @@ import matplotlib.pyplot as plt
 import scipy.optimize
 
 #Tabellenfunktion
-
 a = 24.3124079487
 b = 1.05876485886
 def tabelle(V,a,b):
     return a*V + b
     
-data = np.loadtxt('data_test.txt')
-
+    
+#Konstanten
 eV = 1.6021766208*10**(-19)
 k_B = 1.38064852*10**(-23)
 
-#print(np.exp(1.107*eV/(2*k_B*300))/10000)
 
+#Daten auslesen
+data = np.loadtxt('data_test.txt')
 V_0 = data[:,0] #V
 R = data[:,1] #Ohm
 T_C = data[:,2] #C
@@ -37,14 +37,9 @@ t = data[:,3] #s
 V = 10**3*np.array(V_0) #mV
 T = 273.15+np.array(T_C) #K
 
+
+#Temperatur aus Spannung bestimmen
 T_0 = 26
-
-A_B_guess = [100,1.107*eV/(2*k_B)]
-
-
-def func(T,A,B):
-    return A*T**(-3/2)*np.exp(B/T)
-    
 T_1 = tabelle(V,a,b)
 
 def Uth(V, T_0,a,b):
@@ -52,16 +47,20 @@ def Uth(V, T_0,a,b):
     return V + uv
 
 T_korr = tabelle(Uth(V,T_0,a,b),a,b)
-
 T_korr = 273.15+np.array(T_korr)
 
-param = scipy.optimize.curve_fit(func,T,R,A_B_guess)[0]
 
+#exponentielle Fitfunktion
+A_B_guess = [100,1.107*eV/(2*k_B)]
+
+def func(T,A,B):
+    return A*T**(-3/2)*np.exp(B/T)
+
+param = scipy.optimize.curve_fit(func,T,R,A_B_guess)[0]
 A_new = float(param[0])
 B_new = float(param[1])
 print(A_new)
 print(B_new)
-
 '''
 plt.figure()
 plt.plot(T,R,'r')
@@ -71,6 +70,7 @@ plt.show()
 '''
 
 
+#lineare Fitfunktion
 def linearfit(x,a,b):
     return a*x + b
 
